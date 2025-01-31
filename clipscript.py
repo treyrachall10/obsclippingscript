a02d7cf47e4a2a23ad15d23f1887c41c13327c68
Trey-Rachall <mathew.rachall@gmail.com>
2025-01-30 22:51:35 -0600

    trey-rachall, created repo for the OBS clipping script

---
+++ clipscript.py
@@ -0,0 +1,65 @@
+import obspython as obs
+import os
+import datetime
+
+operatingSystem = os.name
+homeDir = os.path.join(os.path.expanduser("~"), "Videos")
+
+if not os.path.isdir(homeDir):
+    raise ValueError(f"Directory does not exist: {homeDir}")
+
+# starts recording
+def startRecording():
+    obs.obs_frontend_recording_start()
+
+
+# stops recording
+def stopRecording():
+    obs.obs_frontend_recording_stop()
+
+# gets current file path
+    oldPath = getOldFilePath()
+# gets new file name
+    newName = getNewFileName()
+    assignFileName(newName, oldPath)
+
+
+# gets name of file
+def getNewFileName():
+    category = input("input category")
+    moveName = input("input moveName")
+    newFileName = checkFileName(category, moveName)
+    return newFileName
+
+# checks if file name is already taken
+def checkFileName(category, moveName):
+    counter = 1
+    while True:
+        # arranges new file name
+        newName = f"{category}_{moveName}_{counter}"
+        # arranges file name
+        newFilePath = getNewFilePath(newName)
+        # if name taken then increment the counter
+        if not os.path.exists(newFilePath):
+            return newFilePath
+        counter += 1
+
+# assigns new file name
+def assignFileName(newName, oldPath):
+    os.rename(oldPath, newName)
+
+# returns the current file path
+def getOldFilePath():
+    currentTime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    oldPath = os.path.join(homeDir, f"{currentTime}.mp4")
+    return oldPath
+
+# returns the updated file path
+def getNewFilePath(newName):
+    return os.path.join(homeDir, f"{newName}.mp4")
+
+""" 
+next ensure all the folders are made
+then make it where the files will go to their respective folders ie. exciting in exciting
+then submission in submisson etc.
+"""
